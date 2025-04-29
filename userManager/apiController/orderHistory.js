bac753accf760c312f6a2c657205e7f5bd2b87dc
Minh Lu Luan <minhlu14206@gmail.com>
2025-04-29 22:37:53 +0200

    adjust order history

--- userManager/apiController/orderHistory.js
+++ userManager/apiController/orderHistory.js
@@ -1,13 +1,15 @@
 import { Make_Query } from "../../database/databaseConnection.js";
 
 async function OrderHistory(request, response) {
-    try{
-        const {Email} = request.body;
-        const [get_user_id] = await Make_Query(`SELECT User_id FROM Users WHERE Email = '${Email}'`)
-        let User_id = get_user_id.User_id
-        const get_order_history = await Make_Query(`SELECT * FROM Orders WHERE User_id = ${User_id}`);
 
-        console.info(`Request Order history from ${Email} successfully..`)
+    const {
+        User
+    } = request.body
+
+    try{
+        const get_order_history = await Make_Query(`SELECT * FROM Orders WHERE User_id = ${User.User_id}`);
+
+        console.info(`Request Order history from ${User.Email} successfully..`)
         response.status(200).json({
             success: true,
             message: 'Order history retrieved successfully!',
